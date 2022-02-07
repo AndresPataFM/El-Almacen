96dbd052a6bd7a3131534c02b1fe11f8d3278043
Andrés Pata FM <apfm21@Gmail.com>
2022-02-07 16:21:11 -0300

    CartContext

--- src/components/bases/CartWidget/CartWidget.jsx
+++ src/components/bases/CartWidget/CartWidget.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext } from "react";
 import { CartContext } from "./../../../context/CartContext/CartContext";
 
 import shoppingCart from "./../../../img/ShoppingCart.svg"
