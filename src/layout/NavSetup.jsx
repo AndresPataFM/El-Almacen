cf719fc925c4ae0fb4f8c89aa979f4ac6d53f17b
Andrés Pata FM <apfm21@Gmail.com>
2022-03-06 22:01:33 -0300

    netlify fix

--- src/layout/NavSetup.jsx
+++ src/layout/NavSetup.jsx
@@ -1,13 +1,12 @@
 import React from "react"
-import { Fragment } from "react/cjs/react.development"
 import NavBar from "../components/bases/NavBar/NavBar"
 
 const NavSetup = ()=>{
     const categories = [{name:"Home" ,link:"/" }, {name:"Nosotros" ,link:"/nosotros" }, {name:"Contacto" ,link:"/contacto"}]
     return(
-        <Fragment >
+        <div style={{width: "100%", height: "150px"}}>
             <NavBar categories={categories} />
-        </Fragment>
+        </div>
     )
 }
 export default NavSetup
